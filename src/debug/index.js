93b82712d0fda48ba99287e8b6e1da1f2742e7c3
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-05 13:38:14 -0600

    debugging. herramienta para probar o ver que sucede con el codigo

---
+++ src/debug/index.js
@@ -0,0 +1,11 @@
+var a = 'Hello';
+
+function hello() {
+    let b = 'Hello world';
+    const c = 'Hello world!';
+    debugger;
+}
+
+hello();
+
+debugger // Herramienta que esta en el inspector de elementos en apartado de Sources. Ayuda a ver a detalle la funcion del codigo, scope, errores a detalle, etc.
